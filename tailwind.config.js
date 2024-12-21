3ba5d3f65a528f92046020e042fb1df04f976095
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-21 14:00:30 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html", // Головна сторінка
-    "./src/**/*.{js,ts,jsx,tsx}", // Всі файли у `src`
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
