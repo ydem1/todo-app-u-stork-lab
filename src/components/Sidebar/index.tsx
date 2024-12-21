c1abd0876cb6e525030ae525faab9fe38aa4f0d0
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-21 17:20:05 +0200

    fix

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { Navigation } from "./Navigation";
 
 export const Sidebar = () => (
-  <aside className="min-w-62.5 border-blue-base bg-white-blue h-full rounded-l-3xl border-r-2 pb-20">
+  <aside className="min-w-62.5 border-blue-base bg-white-blue h-full rounded-l-3xl border-r-2 pb-20 pt-6">
     <Navigation />
   </aside>
 );
