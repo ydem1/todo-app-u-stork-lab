ede81008a1abe71322043b54deac190411dca8fe
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-21 14:50:42 +0200

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../Logo";
 import { Navigation } from "./Navigation";
 
 export const Header = () => (
-  <header className="border-2 border-blue-base">
+  <header className="border-b-2 border-blue-base">
     <div className="container flex items-center justify-between py-6">
       <Logo />
       <Navigation />
