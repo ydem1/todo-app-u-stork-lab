7732ed9d74e62a8116581e2d9be0faa9dba731d2
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-12-23 14:28:10 +0200

    fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,8 @@ import { Title } from "src/page-components/home/Title";
 import { PageWrapper } from "src/components/Layouts/PageWrapper";
 
 const Home: FC = () => (
-  <PageWrapper mainClassName="bg-white-blue py-10 dark:bg-blue-base">
-    <section className="container flex h-full flex-col items-center justify-center">
+  <PageWrapper mainClassName="bg-white-blue dark:bg-blue-base">
+    <section className="container flex h-full flex-col items-center justify-center py-10">
       <Title />
       <MainTechnologies />
     </section>
